3a62048dce7cf4d6a16248d54cd8fd0c82ece7b1
GuinhoFSilva <guilherme.silva@sptech.school>
2026-07-16 12:14:58 -0300

    feat(infra): add jwt adapter and security config

---
+++ src/main/java/guinho/olympus/infrastructure/security/JwtTokenExtractor.java
@@ -0,0 +1,38 @@
+package guinho.olympus.infrastructure.security;
+
+import guinho.olympus.core.application.abstractions.TokenExtractor;
+import guinho.olympus.core.domain.match.valueobject.PlayerId;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class JwtTokenExtractor implements TokenExtractor {
+    private final SecretKey signingKey;
+
+    public JwtTokenExtractor() {
+        String secret = System.getenv("JWT_SECRET");
+
+        if(secret == null || secret.isBlank()) {
+            throw new IllegalStateException("JWT_SECRET not defined");
+        }
+
+        this.signingKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public PlayerId extractPlayerId(String token) {
+        Claims claims = getPayload(token);
+        return PlayerId.of(UUID.fromString(claims.getSubject()));
+    }
+
+    private Claims getPayload(String token) {
+        return Jwts.parser()
+                .verifyWith(signingKey)
+                .build()
+                .parseSignedClaims(token).getPayload();
+    }
+}
