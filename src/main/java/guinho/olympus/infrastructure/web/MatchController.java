26352e2d83a7c530e79a082a5b8f366c7b761bc1
GuinhoFSilva <guilherme.silva@sptech.school>
2026-07-17 11:03:24 -0300

    feat(web): add rest controller for Match

---
+++ src/main/java/guinho/olympus/infrastructure/web/MatchController.java
@@ -0,0 +1,58 @@
+package guinho.olympus.infrastructure.web;
+
+import guinho.olympus.core.application.usecase.match.GetMatchUseCase;
+import guinho.olympus.core.application.usecase.match.GetPlayerMatchesUseCase;
+import guinho.olympus.core.application.usecase.match.JoinQueueUseCase;
+import guinho.olympus.core.application.usecase.match.LeaveQueueUseCase;
+import guinho.olympus.core.application.usecase.match.dto.JoinQueueResult;
+import guinho.olympus.core.application.usecase.match.dto.MatchResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/matches")
+public class MatchController {
+    private final GetMatchUseCase getMatchUseCase;
+    private final GetPlayerMatchesUseCase getPlayerMatchesUseCase;
+    private final JoinQueueUseCase joinQueueUseCase;
+    private final LeaveQueueUseCase leaveQueueUseCase;
+
+    public MatchController(GetMatchUseCase getMatchUseCase, GetPlayerMatchesUseCase getPlayerMatchesUseCase, JoinQueueUseCase joinQueueUseCase, LeaveQueueUseCase leaveQueueUseCase) {
+        this.getMatchUseCase = getMatchUseCase;
+        this.getPlayerMatchesUseCase = getPlayerMatchesUseCase;
+        this.joinQueueUseCase = joinQueueUseCase;
+        this.leaveQueueUseCase = leaveQueueUseCase;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<List<MatchResponseDto>> getPlayerMatches(JwtAuthenticationToken token) {
+        return ResponseEntity.ok().body(getPlayerMatchesUseCase.findMatches(token.getToken().getTokenValue()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MatchResponseDto> getMatch(JwtAuthenticationToken token, @PathVariable UUID id) {
+        return ResponseEntity.ok().body(getMatchUseCase.find(token.getToken().getTokenValue(), id));
+    }
+
+    @PostMapping("/queue")
+    public ResponseEntity<JoinQueueResult> joinQueue(JwtAuthenticationToken token) {
+        JoinQueueResult response = joinQueueUseCase.execute(token.getToken().getTokenValue());
+
+        HttpStatus status = HttpStatus.OK;
+
+        if(response.hasMatch()) status = HttpStatus.CREATED;
+
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @DeleteMapping("/queue")
+    public ResponseEntity<Void> leaveQueue(JwtAuthenticationToken token) {
+        leaveQueueUseCase.execute(token.getToken().getTokenValue());
+        return ResponseEntity.noContent().build();
+    }
+}
