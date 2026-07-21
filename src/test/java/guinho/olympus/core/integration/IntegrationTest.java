c64b44b5c7a56f9cc8fdfda0d822c1e8fd56b09d
GuinhoFSilva <guilherme.silva@sptech.school>
2026-07-21 10:17:04 -0300

    test: add IntegrationTest and IntegrationTestContainers classes

---
+++ src/test/java/guinho/olympus/core/integration/IntegrationTest.java
@@ -0,0 +1,23 @@
+package guinho.olympus.core.integration;
+
+import guinho.olympus.HermesApplication;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureWebMvc;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = HermesApplication.class)
+@ExtendWith(IntegrationTestContainers.class)
+@AutoConfigureWebMvc
+@AutoConfigureMockMvc
+@ImportAutoConfiguration
+public @interface IntegrationTest {
+}
---
+++ src/test/java/guinho/olympus/core/integration/IntegrationTestContainers.java
@@ -0,0 +1,29 @@
+package guinho.olympus.core.integration;
+
+import com.redis.testcontainers.RedisContainer;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class IntegrationTestContainers implements BeforeAllCallback {
+    private static final AtomicBoolean CONTAINERS_STARTED = new AtomicBoolean(false);
+    private static final RedisContainer REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis"));
+
+    private static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("test_hermes")
+            .withUsername("test_user")
+            .withPassword("root");
+
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        if (!CONTAINERS_STARTED.get()) {
+            MY_SQL_CONTAINER.start();
+            REDIS_CONTAINER.start();
+            CONTAINERS_STARTED.set(true);
+        }
+    }
+}
